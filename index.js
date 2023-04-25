5c44ae26c1aa56874e0153e8d85c53fedb380e82
Bhanu Pratap <pratap.b2001@gmail.com>
2023-04-25 10:55:39 +0530

    movies site
    A movies site made using css html javascript ...

---
+++ index.js
@@ -0,0 +1,158 @@
+let left_btn=document.querySelector(".fa-chevron-left");
+let right_btn=document.querySelector(".fa-chevron-right");
+let cards=document.querySelector(".cards");
+let search=document.querySelector(".search")
+let search_input=document.getElementById('search_input');
+
+
+left_btn.addEventListener('click',()=>{
+     cards.scrollLeft-=140
+});
+
+right_btn.addEventListener('click',()=>{
+    cards.scrollLeft+=140;
+});
+
+let json_url="movie.json";
+//for popular bar
+fetch(json_url).then(Response=>Response.json()).then((data)=>{
+    data.forEach((ele,i)=>{
+        let {name,imdb,date,sposter,bposter,genre,url}=ele;
+        let card=document.createElement('a');
+        card.classList.add('card');
+        card.href=url;
+        card.innerHTML= `
+        <img src="${sposter}" alt="${name}" class="poster">
+        <div class="rest_card">
+            <img src="${bposter}" alt="">    
+            <div class="cont">
+                <h4>${name}</h4>
+                <div class="sub">
+                    <p>${genre},${date}</p>
+                    <h3 ><span>IMDB</span><i class="fa-solid fa-star" style="color: #ffd500;"></i> ${imdb}</h3>
+                </div>
+            </div>
+        </div> 
+        `
+        cards.appendChild(card);
+    });
+    document.getElementById('title').innerText=data[0].name;
+    document.getElementById('gen').innerText=data[0].genre;
+    document.getElementById('rate').innerHTML=`<span>IMDB</span><i class="fa-solid fa-star" style="color: #ffd500;"></i>${data[0].imdb}`;
+    document.getElementById('date').innerText=data[0].date;
+
+ //for search bar..........
+    data.forEach(ele=>{
+        let {name,imdb,date,sposter,genre,url}=ele;
+        let card=document.createElement('a');
+        card.classList.add('card');
+        card.href=url;
+        card.innerHTML= `
+        <img src="${sposter}">
+         <div class="cont">
+          <h3>${name}</h3>
+            <p>${genre},${date}, <span>IMDB</span><i class="fa-solid fa-star" style="color: #ffd500;"></i>${imdb}</p>
+        </div>`
+        search.appendChild(card);
+    });
+
+        search_input.addEventListener('keyup',()=>{
+          let fliter = search_input.value.toUpperCase();
+           let a=search.getElementsByTagName('a');
+          for(let i=0 ;i<a.length;i++){
+            let b=a[i].querySelector(".cont");
+            let textvalue=b.textvalue || b.innerText;   
+            if(textvalue.toUpperCase().indexOf(fliter)>-1){
+                a[i].style.display="flex";
+                search.style.visibility= "visible";
+                search.style.opacity='1';
+            }
+            else{
+               a[i].style.display="none";
+            }
+            if(search_input.value==0){
+                search.style.visibility= "hidden";
+                search.style.opacity="0";
+            }
+        }
+    }) 
+
+    //play pause
+      let video=document.getElementsByTagName('video')[0];
+      let play=document.getElementById('play');
+       play.addEventListener('click',()=>{
+          if(video.paused){
+            video.play();
+            play.innerHTML=`Paused <i class="fa-solid fa-play" id="playbtn" style="color: #000000;"></i>`
+          }
+          else{
+           video.pause();
+           play.innerHTML=`Watch Now<i class="fa-solid fa-pause" id="playbtn" style="color: #000000;"></i>`
+          }
+
+          //filer by button
+    })
+
+    let Series=document.getElementById('Series');
+    let Movies=document.getElementById('Movies');
+    let Home=document.getElementById('Home');
+
+    Series.addEventListener('click',()=>{
+      cards.innerHTML='';
+
+       let series_array= data.filter(ele => {
+         return ele.type==="series";
+        
+     });
+    
+        series_array.forEach((ele,i) =>{
+        let {name,imdb,date,sposter,bposter,genre,url}=ele;
+        let card=document.createElement('a');
+        card.classList.add('card');
+        card.href=url;
+        card.innerHTML= `
+        <img src="${sposter}" alt="${name}" class="poster">
+        <div class="rest_card">
+            <img src="${bposter}" alt="">    
+            <div class="cont">
+                <h4>${name}</h4>
+                <div class="sub">
+                    <p>${genre},${date}</p>
+                    <h3 ><span>IMDB</span><i class="fa-solid fa-star" style="color: #ffd500;"></i> ${imdb}</h3>
+                </div>
+            </div>
+        </div> 
+        `
+        cards.appendChild(card);
+        });
+    });
+
+    //for movies;
+    Movies.addEventListener('click',()=>{
+        cards.innerHTML='';
+  
+         let movies_array= data.filter(ele => {
+           return ele.type==="movie";
+       });
+       movies_array.forEach((ele,i) =>{
+        let {name,imdb,date,sposter,bposter,genre,url}=ele;
+        let card=document.createElement('a');
+        card.classList.add('card');
+        card.href=url;
+        card.innerHTML= `
+        <img src="${sposter}" alt="${name}" class="poster">
+        <div class="rest_card">
+            <img src="${bposter}" alt="">    
+            <div class="cont">
+                <h4>${name}</h4>
+                <div class="sub">
+                    <p>${genre},${date}</p>
+                    <h3 ><span>IMDB</span><i class="fa-solid fa-star" style="color: #ffd500;"></i> ${imdb}</h3>
+                </div>
+            </div>
+        </div> 
+        `
+        cards.appendChild(card);
+        });
+    });
+})
